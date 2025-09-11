9bfee42aff21d58af440e6135653402841470304
seailo <iams3ailo@gmail.com>
2025-09-11 15:38:45 -0400

    unfinished lab work and exercise

--- lab5_function_object/lab5_Murray.js
+++ lab5_function_object/lab5_Murray.js
@@ -1,1 +1,48 @@
+/**
+ * Andrew Murray
+ * Lab 5, more about functions, object
+ * Sept 11, 2025
+ */ 
+ 
 console.log ('Andrew Murray')
+// annonyumous functions within a variable
+console.log ('------- Example 1 -------')
+let greet = function(username){
+    console.log(`Welcom to function ${username}`)
+
+}
+// arrow function
+let greeting = function(username)=>{
+    console.log(`Good afternoon ${username}`)
+}
+
+//EXAMPLE 2: default parameters
+// randomly generated a number between 1 and 9 
+// function will pass the amount of time that the number will be generated 
+function rand_number(x){
+    for (let n =1; n <=x; n++)
+        let num = Math.ceil(math.random()*9)
+        console.log(`${num}`)
+        
+}
+
+
+    numbers=[2,0,-10,5,8,-9]
+    let max_number = Math.Max(...numbers)
+    console.log(`/n---Example 3: spread ----`)
+    console.log(`The maximum number is = ${max_number}`)
+
+
+    const car = {
+        //Properties
+        type: 'FIAT',
+        model: '500',
+        color: 'white'
+        
+    }
+
+
+    console.log('pleasepass a string next time!')
+    Finally{console.log(End of Program!'')}
+
+    Exercise 1
