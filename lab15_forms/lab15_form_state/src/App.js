3e24ab7063073db594ebc929265b852b033906bb
Seailo <iams3ailo@gmail.com>
2025-11-23 21:07:10 +0000

    finihed 15

--- lab15_forms/lab15_form_state/src/App.js
+++ lab15_forms/lab15_form_state/src/App.js
@@ -2,104 +2,113 @@ import './App.css';
 import React, {useState} from 'react'
 
 function App() {
-  /*
-  //set initial state for username
-  const [username, setUsername] = useState("")  
-  */
   
   // set states for multiple inputs 
   const [inputs, setInputs]= useState({})
-  //function to handle changes in all inputs
 
   //set states for gender
-  const [myGender,setMyGender]=useState({})
+  const [myGender,setMyGender]=useState("")
+
+  //function to handle changes in all inputs
   const handle_changes=function(event){
     const name =event.target.name
     const value =event.target.value
     //update the states
     setInputs(values =>({...values,[name]:value}))
   } 
-//function to collect gender 
-const setMygender = (event)=>{
-  setMygender(event.taget.value)
-}
 
-//set state for comment area
-const[textcomment, setComment]=useState('')
+  //function to collect gender 
+  const selectedGender = function(event){
+    setMyGender(event.target.value)
+  }
+
+  //set state for comment area
+  const[textcomment, setComment]=useState('')
   
-//function to collect the comments
-const submitted_comments= function(event){
-  setComment(event.targe.value)
-}
+  //function to collect the comments
+  const submitted_comments= function(event){
+    setComment(event.target.value)
+  }
 
-//function submit form 
+  //function submit form 
   const submitform= function(event){
     event.preventDefault()
   }
-//pasport function 
+
+  //pasport function 
   return (
     <div className="App">
-     <form className= "myform">
+     <form className= "myform" onSubmit={submitform}>
       <fieldset>
         <legend>Form in React JS</legend>
+
+        {/** Username */}
         <div className='formgroup'>
-        <label for="name">Enter Username
-        </label>
-        <input
-         type="text" 
-         id="name"
-         className='importstyle' 
-         placeholder="Type the username..."
-        value={inputs.username}
-        onChange= {handle_changes}
-        />
+          <label htmlFor="name">Enter Username</label>
+          <input
+           type="text" 
+           id="name"
+           name="username"
+           className='importstyle' 
+           placeholder="Type the username..."
+           value={inputs.username}
+           onChange= {handle_changes}
+          />
         </div>
 
+        {/** Age */}
         <div className='formgroup'>
-          <label for="age">Enter your Age:</label>
+          <label htmlFor="age">Enter your Age:</label>
           <input 
-          type='number'
-          id='age'
-          className='importstyle'
-          name="age"
-          placeholder='Type in number your age...'
-          value={inputs.age}
-          onChange= {handle_changes}
+            type='number'
+            id='age'
+            className='importstyle'
+            name="age"
+            placeholder='Type in number your age...'
+            value={inputs.age}
+            onChange= {handle_changes}
           />
         </div>
         
         {/** Select a Gender*/ }
         <div className="formgroup">
           <label> select a gender:
-        <select value={myGender} onChange={selectedGender}>
-          <option value="female">Female</option>
-          <option value="male">Male</option>
-          <option value="other">Other</option>
+            <select value={myGender} onChange={selectedGender}>
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+              <option value="other">Other</option>
+            </select>
+          </label>
+        </div>
 
-        </select>
-        </label>
+        {/** Password Field (ADDED FOR THE LAB) */}
+        <div className='formgroup'>
+          <label htmlFor="password">Enter Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className='importstyle'
+            placeholder="Type your password..."
+            value={inputs.password}
+            onChange={handle_changes}
+          />
         </div>
 
         {/**comments using text area*/}
         <div className='formgroup'>
-          <label for="comments"> Any suggestions?</label>
+          <label htmlFor="comments"> Any suggestions?</label>
           <textarea
-          id ="comments"
-          className='importstyle'
-          value={textcomment}
-          onChange={submitted_comments}
-          placeholder='Please Type Your comments...'
-          
-          
+            id ="comments"
+            className='importstyle'
+            value={textcomment}
+            onChange={submitted_comments}
+            placeholder='Please Type Your comments...'
           />
         </div>
 
         {/**Submit form*/}
-
         <input type ="submit" className='btnsubmit'/>
-        
-
-
       </fieldset>
     
      </form>
@@ -107,10 +116,11 @@ const submitted_comments= function(event){
      <p className='printresult'>Username = {inputs.username} </p>
      <p className="printresult">Age = {inputs.age}</p>
      <p className='printresult'>Gender= {myGender}</p>
-     <p className="Comments">Comments</p>
+     <p className="printresult">Comments = {textcomment}</p>
+     <p className='printresult'>Password = {inputs.password}</p>
+
     </div>
   );
 }
-
 
 export default App;
