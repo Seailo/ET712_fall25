59a515ba75520f9b93ca1d0008cf92583fc68505
Seailo <iams3ailo@gmail.com>
2025-11-25 21:04:36 +0000

    fixinglab16

---
+++ lab16_shoppingcart/shoppingcart/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import React, {useState} from 'react';
+import ProductList from './components/productlist';
+import Cart from './components/cart';
+
+function App() {
+  const [cart,setCart]= useState([])
+ {/** find if the product already exists in the cart*/}
+
+ const addToCart= (product)=>{
+  const exist =cart.find((item)=>item.id===product.id)
+  {/** if product exits, exist==True, increase the quantity*/ }
+
+  if(exist){
+    setCart(
+      cart.map((item)=>{
+       if(item.id===product.id){
+        return {...item,qty:item.qty+1}
+       }
+      else
+        return item 
+    
+      })
+    )
+  }
+  /*If product does not exist, add new item*/
+else{
+  setCart([...cart,{...product, qty:1}])
+}
+}
+/*To remove items from the cart */
+const removeFromCart =(id)=>{
+  setCart(cart.filter((item)=>item.id !==id))
+}
+ 
+
+  return (
+    <div className="App">
+      <h1 className="apptitle">Welcome to Andrew Murray's Shop</h1>
+      <ProductList addToCart={addToCart}/>
+      <Cart cart ={cart} removeFromCart={removeFromCart}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lab16_shoppingcart/shoppingcart/src/components/productlist.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../App.css"
+import apple from "../images/apple.png"
+import mango from "../images/mango.png"
+import grapes from "../images/grapes.png"
+
+const products =[
+    {id:1, name:"Apple", price: 1, was: 2.5, image:apple},
+    {id:2, name:"Mango", price: 3, was: 5.0, image:mango},
+    {id:3, name:"Grapes", price: 1.5, was: 2.8, image:grapes},
+    
+]
+
+const ProductList = (addToCart)=>{
+return(
+    <>
+    <h2 className="producttitle">Products</h2>
+    <section className="cardcontainer">
+        {
+            products.map(
+            (product)=>(
+
+                <div key={product.id} className="card">
+                    <img src={product.image}/>
+                    <p className="productname">
+                        {product.name}
+                        <s>{product.was}/lb</s>
+                        <span>${product.price}</span>
+                    </p>
+
+                        <button classsName="btn_addcart" onClick={()=>addToCart(product)}>
+                            Add to cart
+                        </button> 
+                </div>
+            )
+            )
+        }
+
+    </section>
+    </>
+)
+}
+
+export default ProductList;
