a452cc6397d91fe959328873fa23150479b7044c
Seailo <iams3ailo@gmail.com>
2025-11-29 04:55:22 +0000

    complete with hover red

--- homework8/src/components/Navbar.js
+++ homework8/src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import storeIcon from "../images/store.png";
+import cartIcon from "../images/cart.png";
 
 function Navbar({ cartCount }) {
   return (
     <nav className="navbar">
-      <Link to="/" className="navbrand">My Store</Link>
+      <Link to="/" className="nav-link1">
+        <img src={storeIcon} className="nav-icon" alt="" />
+        Store
+      </Link>
 
-      <Link to="/cart" className="navcart">
+      <Link to="/cart" className="nav-link2">
+        <img src={cartIcon} className="nav-icon" alt="" />
         Cart ({cartCount})
       </Link>
     </nav>
